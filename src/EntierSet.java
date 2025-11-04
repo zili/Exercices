8756e4597a8891573c7d2f7a00a5435ac3d8596e
zili <hassan.zili@gmail.com>
2025-11-04 15:39:37 +0100

    Implement EntierSet class for positive integer input and statistical calculations

--- src/EntierSet.java
+++ src/EntierSet.java
@@ -1,15 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class EntierSet {
+
+    // Fonction pour effectuer la saisie des valeurs, sortie sur valeurs négatives ou 0
+    public static List<Integer> saisie() {
+        Scanner scan = new Scanner(System.in);
+        List<Integer> nombres =  new ArrayList<>();
+
+        System.out.println("Entrez des entiers strictement positifs. "
+                + "Pour terminer, entrez un nombre négatif ou 0.");
+
+        while (true) {
+            System.out.print("Un entier : ");
+            int entree = scan.nextInt();
+
+            // Condition de sortie : si on entre un nombre négatif ou 0
+            if (entree <= 0) {
+                break;
+            }
+
+            nombres.add(entree);
         }
+
+        scan.close();
+        return nombres;
+    }
+
+    // Fonction pour effectuer les calculs (somme, moyenne, max, min)
+    // en utilisant le Set de nombres qu'on a saisi.
+    public static void calculer(List<Integer> liste) {
+        // Afficher le Set "tel quel"
+        System.out.println("Set de nombres saisi : " + liste);
+
+        // Eviter division par 0 en cas de Set vide
+        if (liste ==null || liste.isEmpty()) {
+            System.out.println("Le Set est vide. Aucun calcul possible.");
+            return;
+        }
+
+        // Calcul de la somme
+        // On la note int, mais on doit faire attention aux débordements (overflow)
+        int somme = 0;
+        for (int nb : liste) {
+            somme += nb;
+        }
+
+        // Calcul de la moyenne
+        double moyenne = (double) somme / liste.size();
+
+        // Calcul des max et min
+        int maxVal = Integer.MIN_VALUE;
+        int minVal = Integer.MAX_VALUE;
+        for (int nb : liste) {
+            if (nb > maxVal) maxVal = nb;
+            if (nb < minVal) minVal = nb;
+        }
+
+        // Affichage des résultats
+        System.out.println("Somme : " + somme);
+        System.out.println("Moyenne : " + moyenne);
+        System.out.println("Max : " + maxVal);
+        System.out.println("Min : " + minVal);
+    }
+
+    // Programme principal
+    //https://github.com/zili/Exercices.git
+    public static void main(String[] args) {
+        // Saisie des valeurs
+        List<Integer> nombres = saisie();
+        // Calcul et affichage des statistiques
+        calculer(nombres);
     }
 }
